93239fbc2e70fe5695e7c301f07692142d8ebea1
Dominic Whelan <dominic.whelan27@gmail.com>
2023-02-24 18:09:45 -0330

    AI version, updated, MIN MAX

--- Connect_Four_AI.py
+++ Connect_Four_AI.py
@@ -1,5 +1,7 @@
 # Dominic Whelan
 # Feb.17, 2023
+# Source: https://www.bing.com/videos/search?&q=connect+4+python&view=detail&mid=CB95654BB572BF9D108ACB95654BB572BF9D108A&FORM=VDRVRV&ru=%2Fvideos%2Fsearch%3Fq%3Dconnect%2B4%2Bpython%26FORM%3DHDRSC6&ajaxhist=0
+
 import random
 import numpy as np
 import pygame
@@ -29,8 +31,8 @@ def create_board():
     return board
 
 
-def drop_peice(board, row, col, peice):
-    board[row][col] = peice
+def drop_piece(board, row, col, piece):
+    board[row][col] = piece
 
 
 def is_valid_location(board, col):
@@ -47,54 +49,154 @@ def print_board(board):
     print(np.flip(board, 0))
 
 
-def winning_move(board, peice):
+def winning_move(board, piece):
     # check horizontal locations for win
     for c in range(COLUMN_COUNT - 3):
         for r in range(ROW_COUNT):
-            if board[r][c] == peice and board[r][c + 1] == peice and board[r][c + 2] == peice and board[r][c + 3] == peice:
+            if board[r][c] == piece and board[r][c + 1] == piece and board[r][c + 2] == piece and board[r][c + 3] == piece:
                 return True
     # Check for vertical locations for the win
     for c in range(COLUMN_COUNT):
         for r in range(ROW_COUNT - 3):
-            if board[r][c] == peice and board[r + 1][c] == peice and board[r + 2][c] == peice and board[r + 3][c] == peice:
+            if board[r][c] == piece and board[r + 1][c] == piece and board[r + 2][c] == piece and board[r + 3][c] == piece:
                 return True
     # Check for positive sloped diagonals
     for c in range(COLUMN_COUNT - 3):
         for r in range(ROW_COUNT - 3):
-            if board[r][c] == peice and board[r + 1][c + 1] == peice and board[r + 2][c + 2] == peice and board[r + 3][c + 3] == peice:
+            if board[r][c] == piece and board[r + 1][c + 1] == piece and board[r + 2][c + 2] == piece and board[r + 3][c + 3] == piece:
                 return True
 
     # Check for negative sloped diagonals
     for c in range(COLUMN_COUNT - 3):
         for r in range(3, ROW_COUNT):
-            if board[r][c] == peice and board[r - 1][c + 1] == peice and board[r - 2][c + 2] == peice and board[r - 3][c + 3] == peice:
+            if board[r][c] == piece and board[r - 1][c + 1] == piece and board[r - 2][c + 2] == piece and board[r - 3][c + 3] == piece:
                 return True
 
 
+def evaluate_window(window, piece):
+    score = -100
+
+    opp_piece = PLAYER_PIECE
+    if piece == PLAYER_PIECE:
+        opp_piece = AI_PIECE
+
+    if window.count(piece) == 4:
+        score += 100
+    elif window.count(piece) == 3 and window.count(BLANK) == 1:
+        score += 10
+    elif window.count(piece) == 2 and window.count(BLANK) == 2:
+        score += 5
+
+    if window.count(opp_piece) == 3 and window.count(BLANK) == 1:
+        score -= 100
+
+    return score
+
+
 def score_position(board, piece):
-    score = 0
+    score = -100
+
+    # score center
+    center_array = [int(i) for i in list(board[:, COLUMN_COUNT//2])]
+    center_count = center_array.count(piece)
+    score += center_count * 8
+
+    # score horizontal
     for r in range(ROW_COUNT):
         row_array = [int(i) for i in list(board[r, :])]
         for c in range(COLUMN_COUNT-3):
             window = row_array[c:c+WINDOW_LENGTH]
-            if window.count(piece) == 4:
-                score += 100
-            elif window.count(piece) == 3 and window.count(BLANK) == 1:
-                score += 10
+            score += evaluate_window(window, piece)
+
+    # score vertical
+    for c in range(COLUMN_COUNT):
+        col_array = [int(i) for i in list(board[:, c])]
+        for r in range(ROW_COUNT-3):
+            window = col_array[r:r+WINDOW_LENGTH]
+            score += evaluate_window(window, piece)
+
+    # score positve slopes
+    for r in range(ROW_COUNT-3):
+        for c in range(COLUMN_COUNT-3):
+            window = [board[r+i][c+i] for i in range(WINDOW_LENGTH)]
+            score += evaluate_window(window, piece)
+
+    # score negative slopes
+    for r in range(ROW_COUNT-3):
+        for c in range(COLUMN_COUNT-3):
+            window = [board[r+3-i][c+i] for i in range(WINDOW_LENGTH)]
+            score += evaluate_window(window, piece)
 
     return score
+
+
+def is_terminal_node(board):
+    return winning_move(board, PLAYER_PIECE) or winning_move(board, AI_PIECE) or len(get_valid_locations(board)) == 0
+
+
+def min_max(board, depth, maximizing_player):
+    valid_locations = get_valid_locations(board)
+    is_terminal = is_terminal_node(board)
+
+    if depth == 0 or is_terminal:
+        if is_terminal:
+            if winning_move(board, AI_PIECE):
+                return None, 100000
+            elif winning_move(board, PLAYER_PIECE):
+                return None, -100000
+            else: # no more moves
+                return None, 0
+        else: # depth is 0
+            return None, score_position(board, AI_PIECE)
+    if maximizing_player:
+        value = -math.inf
+        column = random.choice(valid_locations)
+        for col in valid_locations:
+            row = get_next_open_row(board, col)
+            b_copy = board.copy()
+            drop_piece(b_copy, row, col, AI_PIECE)
+            new_score = min_max(b_copy, depth - 1, False)
+            if new_score > value:
+                value = new_score
+                column = col
+            return column, value
+    else:
+        value = math.inf
+        column = random.choice(valid_locations)
+        for col in valid_locations:
+            row = get_next_open_row(board, col)
+            b_copy = board.copy()
+            drop_piece(b_copy, row, col, PLAYER_PIECE)
+            new_score = min_max(b_copy, depth - 1, True)
+            if new_score < value:
+                value = new_score
+                column = col
+            return column, value
 
 
 def get_valid_locations(board):
     valid_locations = []
     for col in range(COLUMN_COUNT-1):
-        if is_valid_location(board,col):
+        if is_valid_location(board, col):
             valid_locations.append(col)
     return valid_locations
 
 
 def pick_best_move(board, piece):
-    pass
+    valid_locations = get_valid_locations(board)
+    best_score = 0
+    best_col = random.choice(valid_locations)
+
+    for col in valid_locations:
+        row = get_next_open_row(board, col)
+        temp_board = board.copy()
+        drop_piece(temp_board, col, row, piece)
+        score = score_position(temp_board, piece)
+        if score > best_score:
+            best_score = score
+            best_col = col
+
+        return best_col
 
 
 def draw_board(board):
@@ -161,7 +263,7 @@ while not game_over:
 
                 if is_valid_location(board, col):
                     row = get_next_open_row(board, col)
-                    drop_peice(board, row, col, PLAYER_PIECE)
+                    drop_piece(board, row, col, PLAYER_PIECE)
 
                     if winning_move(board, 1):
                         label = myFont.render("Player 1 Wins!!!", 1, RED)
@@ -176,12 +278,13 @@ while not game_over:
 
             # Ask for Player 2 input
     if turn == AI and not game_over:
-        col = random.randint(0, COLUMN_COUNT-1)
+        # col = random.randint(0, COLUMN_COUNT-1)
+        col = pick_best_move(board, AI_PIECE)
 
         if is_valid_location(board, col):
             pygame.time.wait(500)
             row = get_next_open_row(board, col)
-            drop_peice(board, row, col, AI_PIECE)
+            drop_piece(board, row, col, AI_PIECE)
 
             if winning_move(board, 2):
                 label = myFont.render("Player 2 Wins!!!", 1, YELLOW)
